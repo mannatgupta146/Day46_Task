3c224083ca902fce2595efb5857f472298ca8f42
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-20 23:00:22 +0530

    completed

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ let img = document.querySelector('#img')
 
 img.addEventListener('dblclick', function(){
     heart.style.opacity = 1
-    heart.style.transform = 'translate(-50%,-50%) scale(2)'
+    heart.style.transform = 'translate(-50%,-50%) scale(2) rotate(0deg)'
+
+    setTimeout(function(){
+        heart.style.transform = 'translate(-50%,-600%) scale(1.2) rotate(60deg)'
+    }, 800)
+    
+    setTimeout(function(){
+        heart.style.opacity = 0
+    }, 1000)
+
+    setTimeout(function () {
+        heart.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
 })
